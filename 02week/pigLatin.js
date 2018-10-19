f0f1945a1719eae021150832412db9675bf6e2c0
Kellyn <bkellyn013@gmail.com>
2018-10-18 20:05:38 -0500

    first try at pigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -7,10 +7,64 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
+// 
+// - Rules for pigLatin:
+// - check if input is a string
+// - change string to all lowercase.
+// - check if first letter is a vowel. 
+// - If a vowel, Return True. If true, return "yay" function to word. IF false, go to function consonant.
+// - Identify consonants. For loop until you find a vowel.
+// - Find a vowel. splice the consonants.
+// - Add consonants to end of word
+// - return word
+
+// functions:
+// 1. checkValidity (checks input for a string)
+// 2. firstLetterVowel (check if first letter is a vowel)
+// 3. checkforconst (for loop identifying all consonants until return vowel. chops up word into new word) 
 
 function pigLatin(word) {
+  const splitInput = word.split('');
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-  // Your code here
+  const checkValidity = (word) => {
+    if (typeof word === 'string') {
+      return true;
+    }
+  }
+
+  const firstLetterVowel = (word) => {
+    for (let i = 0; i < vowels.length; i++) {
+      if (splitInput[0] == vowels[i]) {
+      return (word + "yay");
+      }
+    }
+  }
+
+   
+    const checkForConst = () => {
+      for (let w = 0; w < splitInput.length; w++) {
+       for (let i = 0; i < vowels.length; i++) {
+          if (splitInput[w] !== vowels[i]) {
+            const removeConstant = splitInput.slice(w+1);
+            removeConstant.push(splitInput[w]);
+            return removeConstant.join('') + 'ay';
+          } 
+        }
+      }
+    }
+
+  if (checkValidity(word)) {
+    if(!firstLetterVowel(word)) {
+     checkForConst();
+      } else {
+      firstLetterVowel(word)
+      }
+    } else {
+    return "invalid move"
+  }
+
+  
 
 }
 
