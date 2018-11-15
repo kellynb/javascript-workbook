cdc57ab0f564b1115cffdfd837c84c2fc06a0498
Kellyn <bkellyn013@gmail.com>
2018-11-15 16:17:15 -0600

    updated vertical win

--- 03week/ticTacToe.js
+++ 03week/ticTacToe.js
@@ -40,6 +40,7 @@ const changeBoard = (row,column) => {
   return board;
 }
 
+// for horizontalWin
 const xValues = (item) => {
   return item.includes('X')
 };
@@ -49,39 +50,39 @@ const oValues = (item) => {
 };
 
 function horizontalWin() {
-  if (board[0].every(xValues)  || board[0].every(oValues))   {
-    return true;
-  } else if (board[1].every(xValues) || board[1].every(oValues)) {
-    return true;
-  } else if (board[2].every(xValues) || board[2].every(oValues)) {
-    return true;
-  }
+  return board.some((item) => {
+    return item.every(xValues) || item.every(oValues);
+  }); 
 }
 
-function verticalWin() {
-  if ((board[0][0] == 'X' && board[1][0] == 'X' && board[2][0] == 'X') || (board[0][0] == 'O' && board[1][0] == 'O' && board[2][0] == 'O'))   {
-    return true;
-  } else if ((board[0][1] == 'X' && board[1][1] == 'X' && board[2][1] == 'X') || (board[0][1] == 'O' && board[1][1] == 'O' && board[2][1] == 'O')) {
-    return true;
-  } else if ((board[0][2] == 'X' && board[1][2] == 'X' && board[2][2] == 'X') || (board[0][2] == 'O' && board[1][2] == 'O' && board[2][2] == 'O')) {
-    return true;
-  }
+// for verticalWin()
+const containIndexValue = (num, value) => {
+  const isTrue = board.every((arr1) => {
+   return arr1[num] == value;
+ })
+  return isTrue;
+}
+
+const verticalWin = () => {
+ return (containIndexValue(0, 'O') || containIndexValue(1, 'O') || containIndexValue(2, 'O')) ||  
+        (containIndexValue(0, 'X') || containIndexValue(1, 'X') || containIndexValue(2, 'X'));
 }
 
 function diagonalWin() {
-  if ((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') || (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O'))   {
-    return true;
-  } else if ((board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') || (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')) {
-    return true;
-  }
+  return ((board[0][0] == 'X' && board[1][1] == 'X' && board[2][2] == 'X') || (board[0][0] == 'O' && board[1][1] == 'O' && board[2][2] == 'O')) ||
+         ((board[0][2] == 'X' && board[1][1] == 'X' && board[2][0] == 'X') || (board[0][2] == 'O' && board[1][1] == 'O' && board[2][0] == 'O')) 
+  
 }
 
+// for tieGame
 const fullBoard  = (item) => {
-  return item.includes('X') || item.includes('O');
+  return !item.includes(' ');
 }
 
 const tieGame = () => {
-  return board[0].every(fullBoard) && board[1].every(fullBoard) && board[2].every(fullBoard)
+  return board.every((item)=> {
+    return item.every(fullBoard);
+  })
 }
 
 
