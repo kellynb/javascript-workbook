deb35876d61a84f6283d08986717bb3445c4fc74
Kellyn <bkellyn013@gmail.com>
2018-11-15 13:02:14 -0600

    update to Towers

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -42,56 +42,56 @@ const isEntryValid = (startStack,endStack) => {
   return validEntry.includes(startStack) && validEntry.includes(endStack);
 };
 
+// function that determines move is valid and calls movePiece if true.
+const isLegal = (currentLocation, endLocation) => {
+  const lastArrayItem = endLocation[endLocation.length - 1];
+  const firstArrayItem = currentLocation[currentLocation.length -1];  
+  return (lastArrayItem > firstArrayItem) || (endLocation.length < 1);
+}
+
+// function to remove the last item in the array called by startStack
+const removedItem = (currentLocation) => {
+  return currentLocation.pop();
+} 
+
+// function that pushes the removedItem onto the variable endLocation
+const movePiece = (currentLocation, endLocation) => {
+  endLocation.push(removedItem(currentLocation));
+}
+// function that puts all values in array a
+const resetGame = (endLocation) => {
+  const moveArray = endLocation.splice(0,4);
+  stacks['a'] = moveArray;
+}
+
+// sees if all values are in stack c. Returns true or false
+const checkForWin = (endStack, endLocation) => {
+  return (endStack == 'c'&& endLocation.length == 4)  
+}
+
 
 function towersOfHanoi(startStack, endStack) {
   // two variables to store the stacks called by the user
   const currentLocation = stacks[startStack];
   const endLocation = stacks[endStack];
 
-  // function that determines move is valid and calls movePiece if true. 
-  function isLegal() {
-  // two variables. lastArrayItem stores the value called by the user in endStack
-  // removedItem stores the value called by the user in startStack
-    const lastArrayItem = endLocation[endLocation.length - 1];
-    const removedItem = currentLocation.pop();
-    
-    // function that pushes the removedItem onto the variable endLocation
-    function movePiece() {
-      endLocation.push(removedItem);
-      return stacks;
-    }
-  
-    if ((lastArrayItem > removedItem) || (endLocation.length < 1)) {
-      return movePiece();
+  // gameplay
+    if (isEntryValid(startStack, endStack)) {
+      if (isLegal(currentLocation, endLocation)) {
+        movePiece(currentLocation, endLocation);
+        if (checkForWin(endStack, endLocation)) {
+          console.log('Winner Winner Chicken Dinner');
+          resetGame(endLocation);
+        }
+      } else {
+        console.log("Please enter a valid move");
+      }
     } else {
-      currentLocation.push(removedItem);
-      console.log('Invalid Move');
-      return stacks;
-    }
+      console.log("Please enter a, b, or c"); 
+    }   
+   
   }
   
-  // function that puts all values in array a
-  const resetGame = () => {
-    const moveArray = endLocation.splice(0,4);
-    stacks['a'] = moveArray;
-  }
-  // function that runs after each movePiece and sees if all values are in stack c. Returns a string and resetGame();
-  const checkForWin = (endStack) => {
-    if (endStack == 'c'&& endLocation.length == 4) {
-      console.log('Winner Winner Chicken Dinner')
-      resetGame();
-    } 
-  }
-  
-
-  if (isEntryValid(startStack,endStack)) {
-    isLegal();
-    } else {
-     console.log ('not valid entry'); 
-    } 
- checkForWin(endStack);
-
-}
 
 function getPrompt() {
   printStacks();
