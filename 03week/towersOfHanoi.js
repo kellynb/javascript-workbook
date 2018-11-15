2bf6b16408a58617969ae8271bcd670b8551bbe6
Kellyn <bkellyn013@gmail.com>
2018-11-15 13:02:14 -0600

    merge changes for towers

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -19,98 +19,61 @@ function printStacks() {
   console.log("c: " + stacks.c);
 }
 
-// const validEntry = ['a', 'b', 'c'];
-// const currentLocation = stacks[startStack];
-// const endLocation = stacks[endStack];
-// const removedItem = currentLocation.pop();
+const validEntry = ['a', 'b', 'c'];
 
+const isEntryValid = (startStack,endStack) => {
+  return validEntry.includes(startStack) && validEntry.includes(endStack);
+};
 
+// function that determines move is valid and calls movePiece if true.
+const isLegal = (currentLocation, endLocation) => {
+  const lastArrayItem = endLocation[endLocation.length - 1];
+  const firstArrayItem = currentLocation[currentLocation.length -1];  
+  return (lastArrayItem > firstArrayItem) || (endLocation.length < 1);
+}
 
-// const isEntryValid = (startStack,endStack) => {
-//   return validEntry.includes(startStack) && validEntry.includes(endStack);
-// };
+// function to remove the last item in the array called by startStack
+const removedItem = (currentLocation) => {
+  return currentLocation.pop();
+} 
 
-// function movePiece() {
-//   endLocation.push(removedItem);
-//   return stacks;
-// }
+// function that pushes the removedItem onto the variable endLocation
+const movePiece = (currentLocation, endLocation) => {
+  endLocation.push(removedItem(currentLocation));
+}
+// function that puts all values in array a
+const resetGame = (endLocation) => {
+  const moveArray = endLocation.splice(0,4);
+  stacks['a'] = moveArray;
+}
 
-// function isLegal() {
-//   const lastArrayItem = endLocation[endLocation.length - 1];
-//   if ((lastArrayItem > removedItem) || (endLocation.length < 1)) {
-//     return movePiece();
-//   } else {
-//     currentLocation.push(removedItem);
-//     console.log('Invalid Move');
-//     return stacks;
-//   }
-// }
-
-// const resetGame = () => {
-//   const moveArray = endLocation.splice(0,4);
-//   stacks['a'] = moveArray;
-// }
-
-// const checkForWin = (endStack) => {
-//   if (endStack == 'c'&& endLocation.length == 4) {
-//     console.log('Winner Winner Chicken Dinner')
-//     resetGame();
-//   } 
-// }
-
-
-
+// sees if all values are in stack c. Returns true or false
+const checkForWin = (endStack, endLocation) => {
+  return (endStack == 'c'&& endLocation.length == 4)  
+}
 
 
 function towersOfHanoi(startStack, endStack) {
-  const validEntry = ['a', 'b', 'c'];
   const currentLocation = stacks[startStack];
   const endLocation = stacks[endStack];
-  
-  
-  
-  const isEntryValid = (startStack,endStack) => {
-    return validEntry.includes(startStack) && validEntry.includes(endStack);
-  };
-  
-  
-  
-  function isLegal() {
-    const lastArrayItem = endLocation[endLocation.length - 1];
-    const removedItem = currentLocation.pop();
-    function movePiece() {
-      endLocation.push(removedItem);
-      return stacks;
-    }
 
-    if ((lastArrayItem > removedItem) || (endLocation.length < 1)) {
-      return movePiece();
+  // gameplay
+    if (isEntryValid(startStack, endStack)) {
+      if (isLegal(currentLocation, endLocation)) {
+        movePiece(currentLocation, endLocation);
+        if (checkForWin(endStack, endLocation)) {
+          console.log('Winner Winner Chicken Dinner');
+          resetGame(endLocation);
+        }
+      } else {
+        console.log("Please enter a valid move");
+      }
     } else {
-      currentLocation.push(removedItem);
-      console.log('Invalid Move');
-      return stacks;
-    }
+      console.log("Please enter a, b, or c"); 
+    }   
+   
   }
   
-  const resetGame = () => {
-    const moveArray = endLocation.splice(0,4);
-    stacks['a'] = moveArray;
-  }
-  
-  const checkForWin = (endStack) => {
-    if (endStack == 'c'&& endLocation.length == 4) {
-      console.log('Winner Winner Chicken Dinner')
-      resetGame();
-    } 
-  }
-  
-  if (isEntryValid(startStack,endStack)) {
-    isLegal();
-    } else {
-     console.log ('not valid entry'); 
-    } 
-  checkForWin(endStack);
-}
 
 function getPrompt() {
   printStacks();
