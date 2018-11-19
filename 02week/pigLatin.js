ea367c360f54d208169909c442adbdb6cb47c71d
Kellyn <bkellyn013@gmail.com>
2018-11-19 12:16:12 -0600

    update to pigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -18,21 +18,20 @@ const rl = readline.createInterface({
 // - Add consonants to end of word
 // - return word
 
-// functions:
-// 1. checkValidity (checks input for a string)
-// 2. firstLetterVowel (check if first letter is a vowel)
-// 3. checkforconst (for loop identifying all consonants until return vowel. chops up word into new word) 
 
 
-
+// arrary of vowels
 const vowels = ['a', 'e', 'i','o', 'u'];
 
+
+// Function to check if first letter is a vowel and return the new recombined word
 const firstLetterVowelWord = (scrubWord, evaluateWord) => {
   if (vowels.includes(evaluateWord[0])) {
     return scrubWord + "yay";
   }
 }
 
+// Function that returns the new word if its a word that starts without a vowel and contains a vowel
 const firstLetterConstWord = (evaluateWord, findFirstVowel) => {
   const vowelIndex = evaluateWord.indexOf(findFirstVowel);
   const constOnly = evaluateWord.slice(0, vowelIndex);
@@ -42,14 +41,19 @@ const firstLetterConstWord = (evaluateWord, findFirstVowel) => {
 };
 
 function pigLatin(word) {
+  // variables that in the end stores the trimmed and lowercased word
   const trimedWord = word.trim();
   const scrubWord = trimedWord.toLowerCase();
   const evaluateWord = scrubWord.split('');
 
+  // Variable to determine if there are no vowels in word and return true or false.
+  // not sure if it should just be a function
   const noVowelInWord = evaluateWord.every((letter) => {
     return !vowels.includes(letter);
   })
 
+  // Variable to find the first vowel in word
+  // not sure if it should just be a function
   const findFirstVowel = evaluateWord.find((vowel) => {
     return vowels.includes(vowel);
   })
