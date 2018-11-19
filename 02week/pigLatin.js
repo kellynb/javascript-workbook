f55323e3eddbd70a3d1555fcca0540d21ad1502d
Kellyn <bkellyn013@gmail.com>
2018-11-19 12:28:30 -0600

    update to PigLatin

--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -18,56 +18,51 @@ const rl = readline.createInterface({
 // - Add consonants to end of word
 // - return word
 
-// functions:
-// 1. checkValidity (checks input for a string)
-// 2. firstLetterVowel (check if first letter is a vowel)
-// 3. checkforconst (for loop identifying all consonants until return vowel. chops up word into new word) 
+// arrary of vowels
+const vowels = ['a', 'e', 'i','o', 'u'];
 
-function pigLatin(word) {
-  const splitInput = word.split('');
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
 
-  const checkValidity = (word) => {
-    if (typeof word === 'string') {
-      return true;
-    }
+// Function to check if first letter is a vowel and return the new recombined word
+const firstLetterVowelWord = (scrubWord, evaluateWord) => {
+  if (vowels.includes(evaluateWord[0])) {
+    return scrubWord + "yay";
   }
-
-  const firstLetterVowel = (word) => {
-    for (let i = 0; i < vowels.length; i++) {
-      if (splitInput[0] == vowels[i]) {
-      return (word + "yay");
-      }
-    }
-  }
-
-   
-    const checkForConst = () => {
-      for (let w = 0; w < splitInput.length; w++) {
-       for (let i = 0; i < vowels.length; i++) {
-          if (splitInput[w] !== vowels[i]) {
-            const removeConstant = splitInput.slice(w+1);
-            removeConstant.push(splitInput[w]);
-            return removeConstant.join('') + 'ay';
-          } 
-        }
-      }
-    }
-
-  if (checkValidity(word)) {
-    if(!firstLetterVowel(word)) {
-     checkForConst();
-      } else {
-      firstLetterVowel(word)
-      }
-    } else {
-    return "invalid move"
-  }
-
-  
-
 }
 
+// Function that returns the new word if its a word that starts without a vowel and contains a vowel
+const firstLetterConstWord = (evaluateWord, findFirstVowel) => {
+  const vowelIndex = evaluateWord.indexOf(findFirstVowel);
+  const constOnly = evaluateWord.slice(0, vowelIndex);
+  const restOfWord = evaluateWord.slice(vowelIndex);
+  const fullNewWord = restOfWord.concat(constOnly);
+  return fullNewWord.join('') + 'ay';
+};
+
+function pigLatin(word) {
+  // variables that in the end stores the trimmed and lowercased word
+  const trimedWord = word.trim();
+  const scrubWord = trimedWord.toLowerCase();
+  const evaluateWord = scrubWord.split('');
+
+  // Variable to determine if there are no vowels in word and return true or false.
+  // not sure if it should just be a function
+  const noVowelInWord = evaluateWord.every((letter) => {
+    return !vowels.includes(letter);
+  })
+
+  // Variable to find the first vowel in word
+  // not sure if it should just be a function
+  const findFirstVowel = evaluateWord.find((vowel) => {
+    return vowels.includes(vowel);
+  })
+
+  if (noVowelInWord) {
+    return scrubWord + 'ay';
+  } else {
+    return firstLetterVowelWord(scrubWord, evaluateWord) || firstLetterConstWord(evaluateWord, findFirstVowel);
+  }
+
+}
 
 function getPrompt() {
   rl.question('word ', (answer) => {
